f955206a96708e3c3682d19996400ad9e5392b73
Michael Sharman <sharmo@gmail.com>
2013-11-20 22:16:28 +1100

    [FEATURE] Added favicon

--- src/includes/header.php
+++ src/includes/header.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <title>Learnosity API Demos</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="./static/images/favicon.ico" type="image/x-icon">
     <link rel="stylesheet" href="./static/vendor/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="./static/vendor/bootstrap/css/bootstrap-theme.min.css">
     <link rel="stylesheet" href="./static/vendor/reveal/reveal.css">
