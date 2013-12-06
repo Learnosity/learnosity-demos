306f8f55451e135f229861bfdbad66c3659b63f3
Ralf Dauenhauer <ralf.dauenhauer@learnosity.com>
2013-12-06 16:47:32 +1100

    [FEATURE] Added questions api "features" demo page.

--- www/assessment/items/index.php
+++ www/assessment/items/index.php
@@ -1,54 +1,6 @@
 <?php
 
-include_once '../../config.php';
-include_once '../../../src/utils/uuid.php';
-include_once '../../../src/utils/RequestHelper.php';
 include_once '../../../src/includes/header.php';
-
-$security = array(
-    "consumer_key" => $consumer_key,
-    "domain"       => $domain,
-    "timestamp"    => $timestamp
-);
-
-$request = array(
-    "user_id"        => $studentid,
-    "rendering_type" => "assess",
-    "name"           => "Items API demo - assess activity.",
-    "state"          => "initial",
-    "activity_id"    => "itemsassessdemo",
-    "session_id"     => UUID::generateUuid(),
-    "course_id"      => $courseid,
-    "items"          => array("ccore_video_260_classification", "ccore_parcc_tecr_grade3"),
-    "type"           => "submit_practice",
-    "config"         => array(
-        "subtitle"   => "Walter White",
-        "navigation" => array(
-            "show_intro"     => true,
-            "show_itemcount" => true,
-            "scroll_to_top"  => false,
-            "scroll_to_test" => false
-        ),
-        "time" => array(
-            "max_time"     => 1500,
-            "limit_type"   => "soft",
-            "show_pause"   => true,
-            "warning_time" => 60,
-            "show_time"    => true
-        ),
-        "renderSaveButton"  => true,
-        "ignore_validation" => false
-    )
-);
-
-$RequestHelper = new RequestHelper(
-    'items',
-    $security,
-    $consumer_secret,
-    $request
-);
-
-$signedActivity = $RequestHelper->generateRequest();
 
 ?>
 
@@ -66,7 +18,6 @@ $signedActivity = $RequestHelper->generateRequest();
     </div>
 </div>
 
-<!-- Container for the items api to load into -->
 <div class="container">
     <div class="row">
         <div class="col-md-12">
