984ec3dc6beeea543dc0bfeefe8560662ddb35db
Ralf Dauenhauer <ralf.dauenhauer@learnosity.com>
2013-12-11 10:51:06 +1100

    [FEATURE] Added "idle" timeout options to the override settings modal on Items API demo 

--- src/views/modals/settings-items.php
+++ src/views/modals/settings-items.php
@@ -25,7 +25,7 @@
                         <div class="panel-heading">Title Settings</div>
                         <div class="panel-body">
                             <div class="form-group">
-                                <label for="show_title" class="col-sm-2 control-label">Show Title?</label>
+                                <label for="show_title" class="col-sm-2 control-label">Show Title</label>
                                 <div class="col-sm-10">
                                     <input type="radio" name="navigation[show_title]" value="true"<?php if (isset($nav['show_title']) && $nav['show_title'] === true) { echo ' checked'; }; ?>> True
                                     <input type="radio" name="navigation[show_title]" value="false"<?php if (isset($nav['show_title']) && $nav['show_title'] === false) { echo ' checked'; }; ?>> False
@@ -52,28 +52,28 @@
 
                             <div class="col-lg-6">
                                 <div class="form-group">
-                                    <label for="show_prev" class="col-sm-6 control-label">Show Previous?</label>
+                                    <label for="show_prev" class="col-sm-6 control-label">Show Previous</label>
                                     <div class="col-sm-6">
                                         <input type="radio" name="navigation[show_prev]" value="true"<?php if (isset($nav['show_prev']) && $nav['show_prev'] === true) { echo ' checked'; }; ?>> True
                                         <input type="radio" name="navigation[show_prev]" value="false"<?php if (isset($nav['show_prev']) && $nav['show_prev'] === false) { echo ' checked'; }; ?>> False
                                     </div>
                                 </div>
                                 <div class="form-group">
-                                    <label for="show_next" class="col-sm-6 control-label">Show Next?</label>
+                                    <label for="show_next" class="col-sm-6 control-label">Show Next</label>
                                     <div class="col-sm-6">
                                         <input type="radio" name="navigation[show_next]" value="true"<?php if (isset($nav['show_next']) && $nav['show_next'] === true) { echo ' checked'; }; ?>> True
                                         <input type="radio" name="navigation[show_next]" value="false"<?php if (isset($nav['show_next']) && $nav['show_next'] === false) { echo ' checked'; }; ?>> False
                                     </div>
                                 </div>
                                 <div class="form-group">
-                                    <label for="show_save" class="col-sm-6 control-label">Show Save?</label>
+                                    <label for="show_save" class="col-sm-6 control-label">Show Save</label>
                                     <div class="col-sm-6">
                                         <input type="radio" name="navigation[show_save]" value="true"<?php if (isset($nav['show_save']) && $nav['show_save'] === true) { echo ' checked'; }; ?>> True
                                         <input type="radio" name="navigation[show_save]" value="false"<?php if (isset($nav['show_save']) && $nav['show_save'] === false) { echo ' checked'; }; ?>> False
                                     </div>
                                 </div>
                                 <div class="form-group">
-                                    <label for="show_submit" class="col-sm-6 control-label">Show Submit?</label>
+                                    <label for="show_submit" class="col-sm-6 control-label">Show Submit</label>
                                     <div class="col-sm-6">
                                         <input type="radio" name="navigation[show_submit]" value="true"<?php if (isset($nav['show_submit']) && $nav['show_submit'] === true) { echo ' checked'; }; ?>> True
                                         <input type="radio" name="navigation[show_submit]" value="false"<?php if (isset($nav['show_submit']) && $nav['show_submit'] === false) { echo ' checked'; }; ?>> False
@@ -82,21 +82,21 @@
                             </div>
                             <div class="col-lg-6">
                                 <div class="form-group">
-                                    <label for="show_fullscreencontrol" class="col-sm-6 control-label">Show Fullscreen?</label>
+                                    <label for="show_fullscreencontrol" class="col-sm-6 control-label">Show Fullscreen</label>
                                     <div class="col-sm-6">
                                         <input type="radio" name="navigation[show_fullscreencontrol]" value="true"<?php if (isset($nav['show_fullscreencontrol']) && $nav['show_fullscreencontrol'] === true) { echo ' checked'; }; ?>> True
                                         <input type="radio" name="navigation[show_fullscreencontrol]" value="false"<?php if (isset($nav['show_fullscreencontrol']) && $nav['show_fullscreencontrol'] === false) { echo ' checked'; }; ?>> False
                                     </div>
                                 </div>
                                 <div class="form-group">
-                                    <label for="show_calculator" class="col-sm-6 control-label">Show Calculator?</label>
+                                    <label for="show_calculator" class="col-sm-6 control-label">Show Calculator</label>
                                     <div class="col-sm-6">
                                         <input type="radio" name="navigation[show_calculator]" value="true"<?php if (isset($nav['show_calculator']) && $nav['show_calculator'] === true) { echo ' checked'; }; ?>> True
                                         <input type="radio" name="navigation[show_calculator]" value="false"<?php if (isset($nav['show_calculator']) && $nav['show_calculator'] === false) { echo ' checked'; }; ?>> False
                                     </div>
                                 </div>
                                 <div class="form-group">
-                                    <label for="toc" class="col-sm-6 control-label">Show Table of Contents?</label>
+                                    <label for="toc" class="col-sm-6 control-label">Show Table of Contents</label>
                                     <div class="col-sm-6">
                                         <input type="radio" name="navigation[toc]" value="true"<?php if (isset($nav['toc']) && $nav['toc'] === true) { echo ' checked'; }; ?>> True
                                         <input type="radio" name="navigation[toc]" value="false"<?php if (isset($nav['toc']) && $nav['toc'] === false) { echo ' checked'; }; ?>> False
@@ -115,26 +115,26 @@
                     </div>
 
                     <div class="panel panel-info">
-                        <div class="panel-heading">UI Settings</div>
+                        <div class="panel-heading">UI / Time Settings</div>
                         <div class="panel-body">
 
                             <div class="col-lg-6">
                                 <div class="form-group">
-                                    <label for="show_progress" class="col-sm-6 control-label">Show Progress?</label>
+                                    <label for="show_progress" class="col-sm-6 control-label">Show Progress</label>
                                     <div class="col-sm-6">
                                         <input type="radio" name="navigation[show_progress]" value="true"<?php if (isset($nav['show_progress']) && $nav['show_progress'] === true) { echo ' checked'; }; ?>> True
                                         <input type="radio" name="navigation[show_progress]" value="false"<?php if (isset($nav['show_progress']) && $nav['show_progress'] === false) { echo ' checked'; }; ?>> False
                                     </div>
                                 </div>
                                 <div class="form-group">
-                                    <label for="show_time" class="col-sm-6 control-label">Show Time?</label>
+                                    <label for="show_time" class="col-sm-6 control-label">Show Time</label>
                                     <div class="col-sm-6">
                                         <input type="radio" name="time[show_time]" value="true"<?php if (isset($time['show_time']) && $time['show_time'] === true) { echo ' checked'; }; ?>> True
                                         <input type="radio" name="time[show_time]" value="false"<?php if (isset($time['show_time']) && $time['show_time'] === false) { echo ' checked'; }; ?>> False
                                     </div>
                                 </div>
                                 <div class="form-group">
-                                    <label for="show_pause" class="col-sm-6 control-label">Show Pause?</label>
+                                    <label for="show_pause" class="col-sm-6 control-label">Show Pause</label>
                                     <div class="col-sm-6">
                                         <input type="radio" name="time[show_pause]" value="true"<?php if (isset($time['show_pause']) && $time['show_pause'] === true) { echo ' checked'; }; ?>> True
                                         <input type="radio" name="time[show_pause]" value="false"<?php if (isset($time['show_pause']) && $time['show_pause'] === false) { echo ' checked'; }; ?>> False
@@ -146,10 +146,29 @@
                                         <input type="number" min="100" max="1000" step="100" class="form-control" name="time[warning_time]" value="<?php if (isset($time['warning_time'])) { echo $time['warning_time']; }; ?>">
                                     </div>
                                 </div>
+                                <div class="form-group">
+                                    <label for="idle_timeout" class="col-sm-6 control-label">Idle Timout</label>
+                                    <div class="col-sm-6">
+                                         <input type="radio" name="configuration[idle_timeout][use_idle_timeout]" value="true"<?php if (isset($con['configuration']['idle_timeout'])) { echo ' checked'; }; ?>> True
+                                         <input type="radio" name="configuration[idle_timeout][use_idle_timeout]" value="false"<?php if ( isset($con['configuration']['idle_timeout']) === false) { echo ' checked'; }; ?>> False
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <label for="interval" class="col-sm-6 control-label">Interval (sec)</label>
+                                    <div class="col-sm-3">
+                                        <input type="number" min="0" step="1" class="form-control" name="configuration[idle_timeout][interval]" value="<?php if (isset($con['configuration']['idle_timeout']['interval'])) { echo $con['configuration']['idle_timeout']['interval']; } else { echo '0'; } ?>">
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <label for="countdown_time" class="col-sm-6 control-label">Countdown Time (sec)</label>
+                                    <div class="col-sm-3">
+                                        <input type="number" min="0" step="1" class="form-control" name="configuration[idle_timeout][countdown_time]" value="<?php if (isset($con['configuration']['idle_timeout']['countdown_time'])) { echo $con['configuration']['idle_timeout']['countdown_time']; } else { echo '0'; } ?>">
+                                    </div>
+                                </div>
                             </div>
                             <div class="col-lg-6">
                                 <div class="form-group">
-                                    <label for="show_time" class="col-sm-6 control-label">Limit Type?</label>
+                                    <label for="show_time" class="col-sm-6 control-label">Limit Type</label>
                                     <div class="col-sm-6">
                                         <select id="limit_type" name="time[limit_type]">
                                             <option value="soft"<?php if (isset($con['time']['limit_type']) && $con['time']['limit_type'] === 'soft') { echo ' selected'; }; ?>>Soft</option>
