582de3541a7dd888257bc31d0caa33f99869fe11
Michael Sharman <sharmo@gmail.com>
2013-12-16 14:20:24 +1100

    [BUG] Fixed issue where init preview wasn't quoting the keynames
    https://app.asana.com/0/8825147419757/9180841876908

--- www/static/js/main.js
+++ www/static/js/main.js
@@ -10,7 +10,7 @@ library.json = {
         var str = '<span class=json-string>';
         var r = pIndent || '';
         if (pKey) {
-            r = r + key + pKey.replace(/[": ]/g, '') + '</span>: ';
+            r = r + key + pKey.replace(/[: ]/g, '') + '</span>: ';
         }
         if (pVal) {
             r = r + (pVal[0] == '"' ? str : val) + pVal + '</span>';
