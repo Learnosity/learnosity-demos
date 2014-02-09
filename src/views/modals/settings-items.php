c1eaf3afbf13f1b99906660d417cb81ea82008ef
Michael Sharman <sharmo@gmail.com>
2014-02-09 15:59:00 +1100

    [REFACTOR] Re-ordered override options

--- src/views/modals/settings-items.php
+++ src/views/modals/settings-items.php
@@ -22,31 +22,6 @@
                 <form class="form-horizontal" role="form" id="frmSettings" method="post">
 
                     <div class="panel panel-info">
-                        <div class="panel-heading">Title Settings</div>
-                        <div class="panel-body">
-                            <div class="form-group">
-                                <label for="show_title" class="col-sm-2 control-label">Show Title</label>
-                                <div class="col-sm-10">
-                                    <input type="radio" name="navigation[show_title]" value="true"<?php if (isset($nav['show_title']) && $nav['show_title'] === true) { echo ' checked'; }; ?>> True
-                                    <input type="radio" name="navigation[show_title]" value="false"<?php if (isset($nav['show_title']) && $nav['show_title'] === false) { echo ' checked'; }; ?>> False
-                                </div>
-                            </div>
-                            <div class="form-group">
-                                <label for="title" class="col-sm-2 control-label">Activity Title</label>
-                                <div class="col-sm-8">
-                                    <input type="text" class="form-control" name="title" id="title" placeholder="Override the default activity name" value="<?php if (isset($con['title'])) { echo $con['title']; }; ?>">
-                                </div>
-                            </div>
-                            <div class="form-group">
-                                <label for="subtitle" class="col-sm-2 control-label">Activity Subtitle</label>
-                                <div class="col-sm-8">
-                                    <input type="text" class="form-control" name="subtitle" id="subtitle" placeholder="Subtitle of the activity" value="<?php if (isset($con['subtitle'])) { echo $con['subtitle']; }; ?>">
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="panel panel-info">
                         <div class="panel-heading">Navigation/Control Settings</div>
                         <div class="panel-body">
                             <div class="col-lg-6">
@@ -86,10 +61,10 @@
                                     </div>
                                 </div>
                                 <div class="form-group">
-                                    <label for="show_calculator" class="col-sm-6 control-label">Show Calculator</label>
+                                    <label for="show_pause" class="col-sm-6 control-label">Show Pause</label>
                                     <div class="col-sm-6">
-                                        <input type="radio" name="navigation[show_calculator]" value="true"<?php if (isset($nav['show_calculator']) && $nav['show_calculator'] === true) { echo ' checked'; }; ?>> True
-                                        <input type="radio" name="navigation[show_calculator]" value="false"<?php if (isset($nav['show_calculator']) && $nav['show_calculator'] === false) { echo ' checked'; }; ?>> False
+                                        <input type="radio" name="time[show_pause]" value="true"<?php if (isset($time['show_pause']) && $time['show_pause'] === true) { echo ' checked'; }; ?>> True
+                                        <input type="radio" name="time[show_pause]" value="false"<?php if (isset($time['show_pause']) && $time['show_pause'] === false) { echo ' checked'; }; ?>> False
                                     </div>
                                 </div>
                             </div>
@@ -102,10 +77,10 @@
                                     </div>
                                 </div>
                                 <div class="form-group">
-                                    <label for="show_itemcount" class="col-sm-6 control-label">Show Item Count</label>
+                                    <label for="show_calculator" class="col-sm-6 control-label">Show Calculator</label>
                                     <div class="col-sm-6">
-                                        <input type="radio" name="navigation[show_itemcount]" value="true"<?php if (isset($nav['show_itemcount']) && $nav['show_itemcount'] === true) { echo ' checked'; }; ?>> True
-                                        <input type="radio" name="navigation[show_itemcount]" value="false"<?php if (isset($nav['show_itemcount']) && $nav['show_itemcount'] === false) { echo ' checked'; }; ?>> False
+                                        <input type="radio" name="navigation[show_calculator]" value="true"<?php if (isset($nav['show_calculator']) && $nav['show_calculator'] === true) { echo ' checked'; }; ?>> True
+                                        <input type="radio" name="navigation[show_calculator]" value="false"<?php if (isset($nav['show_calculator']) && $nav['show_calculator'] === false) { echo ' checked'; }; ?>> False
                                     </div>
                                 </div>
                                 <div class="form-group">
@@ -133,7 +108,7 @@
 
                             <div class="col-lg-6">
                                 <div class="form-group">
-                                    <label for="show_progress" class="col-sm-6 control-label">Show Progress</label>
+                                    <label for="show_progress" class="col-sm-6 control-label">Show Progress Bar</label>
                                     <div class="col-sm-6">
                                         <input type="radio" name="navigation[show_progress]" value="true"<?php if (isset($nav['show_progress']) && $nav['show_progress'] === true) { echo ' checked'; }; ?>> True
                                         <input type="radio" name="navigation[show_progress]" value="false"<?php if (isset($nav['show_progress']) && $nav['show_progress'] === false) { echo ' checked'; }; ?>> False
@@ -147,10 +122,10 @@
                                     </div>
                                 </div>
                                 <div class="form-group">
-                                    <label for="show_pause" class="col-sm-6 control-label">Show Pause</label>
+                                    <label for="show_itemcount" class="col-sm-6 control-label">Show Item Count</label>
                                     <div class="col-sm-6">
-                                        <input type="radio" name="time[show_pause]" value="true"<?php if (isset($time['show_pause']) && $time['show_pause'] === true) { echo ' checked'; }; ?>> True
-                                        <input type="radio" name="time[show_pause]" value="false"<?php if (isset($time['show_pause']) && $time['show_pause'] === false) { echo ' checked'; }; ?>> False
+                                        <input type="radio" name="navigation[show_itemcount]" value="true"<?php if (isset($nav['show_itemcount']) && $nav['show_itemcount'] === true) { echo ' checked'; }; ?>> True
+                                        <input type="radio" name="navigation[show_itemcount]" value="false"<?php if (isset($nav['show_itemcount']) && $nav['show_itemcount'] === false) { echo ' checked'; }; ?>> False
                                     </div>
                                 </div>
                                 <div class="form-group">
@@ -158,12 +133,6 @@
                                     <div class="col-sm-6">
                                         <input type="radio" name="navigation[scrolling_indicator]" value="true"<?php if (isset($nav['scrolling_indicator']) && $nav['scrolling_indicator'] === true) { echo ' checked'; }; ?>> True
                                         <input type="radio" name="navigation[scrolling_indicator]" value="false"<?php if (isset($nav['scrolling_indicator']) && $nav['scrolling_indicator'] === false) { echo ' checked'; }; ?>> False
-                                    </div>
-                                </div>
-                                <div class="form-group">
-                                    <label for="warning_time" class="col-sm-6 control-label">Warning Time (sec)</label>
-                                    <div class="col-sm-3">
-                                        <input type="number" min="100" max="1000" step="100" class="form-control" name="time[warning_time]" value="<?php if (isset($time['warning_time'])) { echo $time['warning_time']; }; ?>">
                                     </div>
                                 </div>
                                 <div class="form-group">
@@ -218,6 +187,24 @@
                                     </div>
                                 </div>
                                 <div class="form-group">
+                                    <label for="fontsize" class="col-sm-6 control-label">Font Size</label>
+                                    <div class="col-sm-6">
+                                        <select id="fontsize" name="configuration[fontsize]">
+                                            <option value="small"<?php if (isset($con['configuration']['fontsize']) && $con['configuration']['fontsize'] === 'small') { echo ' selected'; }; ?>>Small</option>
+                                            <option value="normal"<?php if (isset($con['configuration']['fontsize']) && $con['configuration']['fontsize'] === 'normal') { echo ' selected'; }; ?>>Normal</option>
+                                            <option value="large"<?php if (isset($con['configuration']['fontsize']) && $con['configuration']['fontsize'] === 'large') { echo ' selected'; }; ?>>Large</option>
+                                            <option value="xlarge"<?php if (isset($con['configuration']['fontsize']) && $con['configuration']['fontsize'] === 'xlarge') { echo ' selected'; }; ?>>X Large</option>
+                                            <option value="xxlarge"<?php if (isset($con['configuration']['fontsize']) && $con['configuration']['fontsize'] === 'xxlarge') { echo ' selected'; }; ?>>XX Large</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <label for="warning_time" class="col-sm-6 control-label">Warning Time (sec)</label>
+                                    <div class="col-sm-3">
+                                        <input type="number" min="100" max="1000" step="100" class="form-control" name="time[warning_time]" value="<?php if (isset($time['warning_time'])) { echo $time['warning_time']; }; ?>">
+                                    </div>
+                                </div>
+                                <div class="form-group">
                                     <label for="transition-speed" class="col-sm-6 control-label">Transition Speed (ms)</label>
                                     <div class="col-sm-3">
                                         <input type="number" min="100" max="1000" step="100" class="form-control" name="navigation[transition_speed]" value="<?php if (isset($nav['transition_speed'])) { echo $nav['transition_speed']; }; ?>">
@@ -232,20 +219,33 @@
                                         <p class="help-block pull-right">eg http://demos.learnosity.com/assessment/items/custom.css</p>
                                     </div>
                                 </div>
-                                <div class="form-group">
-                                    <label for="fontsize" class="col-sm-6 control-label">Font Size</label>
-                                    <div class="col-sm-6">
-                                        <select id="fontsize" name="configuration[fontsize]">
-                                            <option value="small"<?php if (isset($con['configuration']['fontsize']) && $con['configuration']['fontsize'] === 'small') { echo ' selected'; }; ?>>Small</option>
-                                            <option value="normal"<?php if (isset($con['configuration']['fontsize']) && $con['configuration']['fontsize'] === 'normal') { echo ' selected'; }; ?>>Normal</option>
-                                            <option value="large"<?php if (isset($con['configuration']['fontsize']) && $con['configuration']['fontsize'] === 'large') { echo ' selected'; }; ?>>Large</option>
-                                            <option value="xlarge"<?php if (isset($con['configuration']['fontsize']) && $con['configuration']['fontsize'] === 'xlarge') { echo ' selected'; }; ?>>X Large</option>
-                                            <option value="xxlarge"<?php if (isset($con['configuration']['fontsize']) && $con['configuration']['fontsize'] === 'xxlarge') { echo ' selected'; }; ?>>XX Large</option>
-                                        </select>
-                                    </div>
-                                </div>
                             </div>
 
+                        </div>
+                    </div>
+
+                    <div class="panel panel-info">
+                        <div class="panel-heading">Title Settings</div>
+                        <div class="panel-body">
+                            <div class="form-group">
+                                <label for="show_title" class="col-sm-2 control-label">Show Title</label>
+                                <div class="col-sm-10">
+                                    <input type="radio" name="navigation[show_title]" value="true"<?php if (isset($nav['show_title']) && $nav['show_title'] === true) { echo ' checked'; }; ?>> True
+                                    <input type="radio" name="navigation[show_title]" value="false"<?php if (isset($nav['show_title']) && $nav['show_title'] === false) { echo ' checked'; }; ?>> False
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label for="title" class="col-sm-2 control-label">Activity Title</label>
+                                <div class="col-sm-8">
+                                    <input type="text" class="form-control" name="title" id="title" placeholder="Override the default activity name" value="<?php if (isset($con['title'])) { echo $con['title']; }; ?>">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label for="subtitle" class="col-sm-2 control-label">Activity Subtitle</label>
+                                <div class="col-sm-8">
+                                    <input type="text" class="form-control" name="subtitle" id="subtitle" placeholder="Subtitle of the activity" value="<?php if (isset($con['subtitle'])) { echo $con['subtitle']; }; ?>">
+                                </div>
+                            </div>
                         </div>
                     </div>
 
