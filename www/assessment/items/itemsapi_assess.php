d192575a2192e1e2c76cbb0ec2efebf815fdbb6b
Michael Sharman <sharmo@gmail.com>
2014-03-05 14:57:17 +1100

    [REFACTOR] Initing Items API into variable

--- www/assessment/items/itemsapi_assess.php
+++ www/assessment/items/itemsapi_assess.php
@@ -135,7 +135,12 @@ $signedRequest = $RequestHelper->generateRequest();
 <script src="//items.learnosity.com"></script>
 <script>
     var activity = <?php echo $signedRequest; ?>;
-    LearnosityItems.init(activity);
+    var eventOptions = {
+        readyListener: function () {
+            console.log("Learnosity Items API is ready");
+        }
+    };
+    var app = LearnosityItems.init(activity, eventOptions);
 </script>
 
 <?php
