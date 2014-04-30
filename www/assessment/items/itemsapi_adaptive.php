8356f352eea0fc623546f4db19ea6c173c0950e4
Michael Sharman <sharmo@gmail.com>
2014-04-30 21:14:42 +1000

    [FEATURE] Added items api activities demo

--- www/assessment/items/itemsapi_adaptive.php
+++ www/assessment/items/itemsapi_adaptive.php
@@ -104,7 +104,7 @@ $signedRequest = $RequestHelper->generateRequest();
                 <span class="glyphicon glyphicon-share-alt"></span> Preview API Initialisation Object
             </a></h4>
         </div>
-        <div class="col-md-2"><p class='text-right'><a class="btn btn-primary btn-lg" href="itemsapi_inline.php">Next <span class="glyphicon glyphicon-chevron-right"></span></a></p></div>
+        <div class="col-md-2"><p class='text-right'><a class="btn btn-primary btn-lg" href="itemsapi_activities.php">Next <span class="glyphicon glyphicon-chevron-right"></span></a></p></div>
     </div>
 </div>
 
