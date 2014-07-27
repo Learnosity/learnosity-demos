8a8c5420642da96c15ad3a5fea668c7ebedf88ac
Michael Sharman <sharmo@gmail.com>
2014-07-27 21:29:32 +1000

    Added new UI to hidden distractor pages

--- www/assessment/items/itemsapi_distractors.php
+++ www/assessment/items/itemsapi_distractors.php
@@ -33,31 +33,28 @@ $signedRequest = $Init->generate();
 
 ?>
 
-<div class="jumbotron">
+<div class="jumbotron section">
+    <div class="pull-right toolbar">
+        <ul class="list-inline">
+            <li data-toggle="tooltip" data-original-title="Visit the documentation"><a href="http://docs.learnosity.com/itemsapi/" title="Documentation"><span class="glyphicon glyphicon-book"></span></a></li>
+            <li data-toggle="tooltip" data-original-title="Next demo"><a href="itemsapi_adaptive.php"><span class="glyphicon glyphicon-circle-arrow-right"></span></a></li>
+        </ul>
+    </div>
     <h1>Items API – Inline</h1>
     <p>Display items from the Learnosity Item Bank in no time with the Items API.  The Items API builds on the Questions API's power and makes it quicker to integrate.<p>
-    <div class="row">
-        <div class="col-md-10">
-            <h4><a href="http://docs.learnosity.com/itemsapi/" class="text-muted">
-                <span class="glyphicon glyphicon-book"></span> Documentation
-            </a></h4>
-            <h4><a href="#" class="text-muted" data-toggle="modal" data-target="#initialisation-preview">
-                <span class="glyphicon glyphicon-share-alt"></span> Preview API Initialisation Object
-            </a></h4>
-        </div>
-        <div class="col-md-2"><p class='text-right'><a class="btn btn-primary btn-lg" href="itemsapi_adaptive.php">Next <span class="glyphicon glyphicon-chevron-right"></span></a></p></div>
-    </div>
 </div>
 
-<p>
-    <span class="learnosity-item" data-reference="act1"></span>
-    <span class="learnosity-item" data-reference="act2"></span>
-    <span class="learnosity-item" data-reference="act3"></span>
-    <span class="learnosity-item" data-reference="act4"></span>
-    <span class="learnosity-item" data-reference="act5"></span>
-    <span class="learnosity-item" data-reference="act6"></span>
-    <span class="learnosity-submit-button"></span>
-</p>
+<div class="section">
+    <p>
+        <span class="learnosity-item" data-reference="act1"></span>
+        <span class="learnosity-item" data-reference="act2"></span>
+        <span class="learnosity-item" data-reference="act3"></span>
+        <span class="learnosity-item" data-reference="act4"></span>
+        <span class="learnosity-item" data-reference="act5"></span>
+        <span class="learnosity-item" data-reference="act6"></span>
+        <span class="learnosity-submit-button"></span>
+    </p>
+</div>
 
 <!-- Container for the items api to load into -->
 <script src="//items.learnosity.com/"></script>
