f11b2ffb76cef9e382f1f4002b04e2df50366a2c
Michael Sharman <sharmo@gmail.com>
2014-07-30 09:32:20 +1000

    Changed double to single quotes

--- www/assessment/items/itemsapi_activities.php
+++ www/assessment/items/itemsapi_activities.php
@@ -18,6 +18,7 @@ $request = array(
     'course_id'            => $courseid,
     'session_id'           => Uuid::generate(),
     'user_id'              => $studentid,
+    'assess_inline'        => false,
     'config'               => array(
         'administration' => array(
             'pwd' => '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8' // `password`
