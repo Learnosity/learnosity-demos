16ecd8903d5487d0e75085ac1a3cc1e468b953f0
Fiona Chan <fiona.chan@learnosity.com>
2014-07-29 18:22:30 +1000

    Remove CSS that causes padding removed on section

--- www/assessment/items/itemsapi_accessibility_showcase.php
+++ www/assessment/items/itemsapi_accessibility_showcase.php
@@ -37,20 +37,6 @@ $signedRequest = $Init->generate();
 
 ?>
 
-<div class="jumbotron section">
-    <div class="pull-right toolbar">
-        <ul class="list-inline">
-            <li data-toggle="tooltip" data-original-title="Customise API Settings"><a href="#" class="text-muted" data-toggle="modal" data-target="#settings"><span class="glyphicon glyphicon-list-alt"></span></a></li>
-            <li data-toggle="tooltip" data-original-title="Preview API Initialisation Object"><a href="#"  data-toggle="modal" data-target="#initialisation-preview"><span class="glyphicon glyphicon-search"></span></a></li>
-            <li data-toggle="tooltip" data-original-title="Visit the documentation"><a href="http://docs.learnosity.com/itemsapi/" title="Documentation"><span class="glyphicon glyphicon-book"></span></a></li>
-            <li data-toggle="tooltip" data-original-title="Next demo"><a href="<?php echo $env['www'] ?>assessment/assess/index.php"><span class="glyphicon glyphicon-circle-arrow-right"></span></a></li>
-        </ul>
-    </div>
-    <h1>Items API – Accessibility Showcase</h1>
-    <p>The Assess API has a built in accessibility options panel that allows students to adjust settings during their assessment. Options include
-    changing the colour scheme, font size and instructions on how to zoom.</p>
-</div>
-
 <div class="section">
     <!-- Container for the items api to load into -->
     <div id="learnosity_assess"></div>
@@ -68,6 +54,6 @@ $signedRequest = $Init->generate();
 </script>
 
 <?php
-    include_once 'views/modals/settings-items.php';
-    include_once 'views/modals/initialisation-preview.php';
+    // include_once 'views/modals/settings-items.php';
+    // include_once 'views/modals/initialisation-preview.php';
     include_once 'includes/footer.php';
