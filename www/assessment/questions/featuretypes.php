435b4a0a32493cae239e6a8e3caeba3ba17a0632
Michael Sharman <sharmo@gmail.com>
2014-10-21 13:02:13 +1100

    Added audio and ruler examples to features demo

--- www/assessment/questions/featuretypes.php
+++ www/assessment/questions/featuretypes.php
@@ -25,10 +25,26 @@ include_once 'includes/header.php';
 
     <section>
         <h3 id="audioplayer">Audio Player</h3>
-        <h4>Example</h4>
+        <h4>Example (Block Player)</h4>
         <span class="learnosity-feature" data-type="audioplayer" data-src="//dw6y82u65ww8h.cloudfront.net/organisations/1/codie_award.mp3" data-waveform="//dw6y82u65ww8h.cloudfront.net/organisations/1/codie_award.png"></span>
         <br />
         <pre class="feature htmlexample">&lt;span class="learnosity-feature" data-type="audioplayer" data-src="audio-source.mp3" data-waveform="//dw6y82u65ww8h.cloudfront.net/demos/docs/waveform.png"&gt;&lt;/span&gt;</pre>
+        <hr />
+    </section>
+
+    <section>
+        <h4>Example (Bar Player)</h4>
+        <span class="learnosity-feature" data-type="audioplayer" data-player="bar" data-src="//dw6y82u65ww8h.cloudfront.net/organisations/1/codie_award.mp3"></span>
+        <br />
+        <pre class="feature htmlexample">&lt;span class="learnosity-feature" data-type="audioplayer" data-player="bar" data-src="audio-source.mp3"&gt;&lt;/span&gt;</pre>
+        <hr />
+    </section>
+
+    <section>
+        <h4>Example (Minimal Player)</h4>
+        <span class="learnosity-feature" data-type="audioplayer" data-player="minimal" data-src="//dw6y82u65ww8h.cloudfront.net/organisations/1/codie_award.mp3"></span>
+        <br />
+        <pre class="feature htmlexample">&lt;span class="learnosity-feature" data-type="audioplayer" data-player="minimal" data-src="audio-source.mp3"&gt;&lt;/span&gt;</pre>
         <hr />
     </section>
 
@@ -59,10 +75,16 @@ include_once 'includes/header.php';
         <h4>Example (Protractor)</h4>
         <span class="learnosity-feature" data-type="imagetool" data-image="protractor"></span>
         <pre style="margin-top:220px" class="feature htmlexample">&lt;span class="learnosity-feature" data-type="imagetool" data-image="protractor"&gt;&lt;/span&gt;</pre>
-        <br/><h4>Example (Ruler 6 inches)</h4>
+        <br>
+        <h4>Example (Ruler 6 inches)</h4>
         <span class="learnosity-feature" data-type="imagetool" data-image="ruler-6-inches"></span>
         <pre style="margin-top: 70px" class="feature htmlexample">&lt;span class="learnosity-feature" data-type="imagetool" data-image="ruler-6-inches"&gt;&lt;/span&gt;</pre>
+        <br>
+        <h4>Example (Ruler 15 cm with button to launch)</h4>
+        <span class="learnosity-feature" data-type="imagetool" data-image="ruler-15-cm" data-button="true"></span>
+        <pre style="margin-top: 20px" class="feature htmlexample">&lt;span class="learnosity-feature" data-type="imagetool" data-image="ruler-15-cm" data-button="true"&gt;&lt;/span&gt;</pre>
     </section>
+
 </div>
 
 <script src="<?php echo $env['www'] ?>static/js/codemirror.min.js"></script>
