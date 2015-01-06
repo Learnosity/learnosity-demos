21cb2383a9a9d72ca4931d11f3e8a71641d2ff5d
Michael Sharman <sharmo@gmail.com>
2015-01-06 19:47:15 +1100

    Deleting SSO demo, it's deprecated and will be removed soon

--- src/includes/nav.php
+++ src/includes/nav.php
@@ -11,8 +11,7 @@
         ),
         'Reporting' => array(
             'reporting/reports/index.php' => 'Reports API',
-            'reporting/data/index.php'    => 'Data API',
-            'reporting/sso/index.php'     => 'Single Sign On API (Deprecated)'
+            'reporting/data/index.php'    => 'Data API'
         ),
         'Misc' => array(
             'misc/security_check.php' => 'Security Check'
