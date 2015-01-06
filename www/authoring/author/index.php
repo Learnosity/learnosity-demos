8f61204b1c6b3aee023e10e7e5055dedb102b281
Michael Sharman <sharmo@gmail.com>
2015-01-06 11:16:14 +1100

    Added index page to access legacy and latest demos

--- www/authoring/author/index.php
+++ www/authoring/author/index.php
@@ -2,78 +2,50 @@
 
 include_once '../../config.php';
 include_once 'includes/header.php';
-
-use LearnositySdk\Request\Init;
-use LearnositySdk\Utils\Uuid;
-
-$item_ref = Uuid::generate();
-
-$security = array(
-    'consumer_key' => $consumer_key,
-    'domain'       => $domain
-);
-
-$request = array(
-    'components' => array(
-        array(
-            'id'        => 'learnosity_author',
-            'type'      => 'itemeditor',
-            'reference' => $item_ref,
-            'question_editor_options' => array(
-                'ui' => array(
-                    'public_methods'     => array(),
-                    'layout'             => '2-column',
-                    'question_tiles'     => false,
-                    'documentation_link' => false,
-                    'change_button'      => true,
-                    'source_button'      => false,
-                    'fixed_preview'      => true,
-                    'advanced_group'     => false,
-                    'search_field'       => false
-                )
-            )
-        )
-    )
-);
-
-$Init = new Init('author', $security, $consumer_secret, $request);
-$signedRequest = $Init->generate();
-
 ?>
 
 <div class="jumbotron section">
-    <div class="toolbar">
+    <div class="pull-right toolbar">
         <ul class="list-inline">
             <li data-toggle="tooltip" data-original-title="Preview API Initialisation Object"><a href="#"  data-toggle="modal" data-target="#initialisation-preview"><span class="glyphicon glyphicon-search"></span></a></li>
             <li data-toggle="tooltip" data-original-title="Visit the documentation"><a href="http://docs.learnosity.com/authorapi/" title="Documentation"><span class="glyphicon glyphicon-book"></span></a></li>
             <li data-toggle="tooltip" data-original-title="Toggle product overview box"><a href="#"><span class="glyphicon glyphicon-chevron-up jumbotron-toggle"></span></a></li>
         </ul>
     </div>
-    <div class="overview">
-        <h1>Author API</h1>
+    <h1>Author API</h1>
+    <div class="section-intro">
         <p>Learnosity's Author API allows searching and integration of Learnosity powered content into your content management system.<p>
-        <p>Below is demo of the Author API editing a new item each time, questions can be created, edited and are persisted to our itembank.</p>
+    </div>
+
+    <div class="row">
+        <div class="col-md-6">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h2 class="panel-title">Latest</h2>
+                </div>
+                <div class="panel-body">
+                    <p>An embedded API enabling content authors to create, edit and persist items, questions and features to our itembank.<p>
+                    <p class="text-right">
+                        <a class="btn btn-primary btn-md" href="./latest.php">Demo</a>
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h2 class="panel-title">Legacy</h2>
+                </div>
+                <div class="panel-body">
+                    <p>Learnosity's Author API allows searching and integration of Learnosity powered content into your content management system.<p>
+                    <p><em>Note:</em> This version is only supported to <a href="http://docs.learnosity.com/authorapi-v0.5/index.php">v0.5</a></p>
+                    <p class="text-right">
+                        <a class="btn btn-primary btn-md" href="./legacy.php">Demo</a>
+                    </p>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
 
-<style>
-    .lrn-nav-tabs {
-        width: auto;
-    }
-</style>
-
-<!-- Container for the items api to load into -->
-<div class="section">
-    <div id="learnosity_author"></div>
-</div>
-
-<script src="//authorapi.learnosity.com?v0.6"></script>
-<script>
-    var initOptions = <?php echo $signedRequest; ?>;
-
-    LearnosityAuthor.init(initOptions);
-</script>
-
-<?php
-    include_once 'views/modals/initialisation-preview.php';
-    include_once 'includes/footer.php';
+<?php include_once 'includes/footer.php';
