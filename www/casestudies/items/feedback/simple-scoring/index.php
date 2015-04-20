31ad09eadce7213a767243cf3d82afb2cae5d0f3
Michael Sharman <sharmo@gmail.com>
2015-04-20 22:52:35 +1000

    Polish and fixes for teacher scoring demos

--- www/casestudies/items/feedback/simple-scoring/index.php
+++ www/casestudies/items/feedback/simple-scoring/index.php
@@ -11,29 +11,24 @@ $security = array(
     'domain'       => $domain
 );
 
-//show in initial state to start and, when reloaded with session id, show in review mode
-if (isset($_GET['session_id'])) {
-    $session_id = $_GET['session_id'];
-    $session_state = 'resume';
-} else {
-    $session_id = Uuid::generate();
-    $session_state = 'initial';
-}
+$session_id    = Uuid::generate();
+$session_state = 'initial';
+$activity_id   = Uuid::generate();
 
 $request = [
-    'user_id'        => $studentid,
-    'rendering_type' => 'assess',
-    'assess_inline'  => true,
-    'name'           => 'Student Assessment demo',
-    'state'          => $session_state,
-    'activity_id'    => 'DemoTest_DemoSiteExample',
-    'session_id'     => $session_id,
-    'course_id'      => 'commoncore',
+    'user_id'              => $studentid,
+    'rendering_type'       => 'assess',
+    'assess_inline'        => true,
+    'name'                 => 'Student Assessment demo',
+    'state'                => $session_state,
+    'activity_id'          => $activity_id,
+    'session_id'           => $session_id,
+    'course_id'            => 'commoncore',
     'activity_template_id' => 'SCORING_DEMO_TEST',
-    'type'           => 'submit_practice',
-    'config'         => array(
+    'type'                 => 'submit_practice',
+    'config'               => array(
         'configuration' => array(
-            'onsubmit_redirect_url' => 'feedback.php?session_id='. $session_id
+            'onsubmit_redirect_url' => 'feedback.php?session_id='. $session_id . '&activity_id='. $activity_id
         )
     )
 ];
@@ -46,14 +41,13 @@ $signedRequest = $Init->generate();
 <div class="jumbotron section">
     <div class="toolbar">
         <ul class="list-inline">
-            <li data-toggle="tooltip" data-original-title="Preview API Initialisation Object"><a href="#"  data-toggle="modal" data-target="#initialisation-preview"><span class="glyphicon glyphicon-search"></span></a></li>
             <li data-toggle="tooltip" data-original-title="Visit the documentation"><a href="http://docs.learnosity.com/itemsapi/" title="Documentation"><span class="glyphicon glyphicon-book"></span></a></li>
         </ul>
     </div>
     <div class="overview">
         <h1>Student Assessment – Step 1</h1>
         <p>Here is a sample student assessment, with a mix of auto and non-autoscorable question types.</p>
-        <p>You will be able to add teacher scoring after completing the assessment.</p>
+        <p>Take the test as a student would, you will be able to add teacher scoring after completing the assessment.</p>
         <div class="previewWrapper preview" style="display: none; height: 300px; overflow: scroll;"><pre><code id="xApiPreview"></code></pre></div>
     </div>
 </div>
@@ -68,12 +62,11 @@ $signedRequest = $Init->generate();
     var eventOptions = {
             readyListener: function () {
                 //add to history to support back button and show in resume mode
-                history.pushState({}, '', window.location.pathname + '?session_id=' + activity.request.session_id);
+                history.pushState({}, '', window.location.pathname + '?session_id=<?php echo $session_id; ?>&activity_id=<?php echo $activity_id; ?>');
             }
         },
         itemsApp = LearnosityItems.init(<?php echo $signedRequest; ?>, eventOptions);
 </script>
 
 <?php
-    include_once 'views/modals/initialisation-preview.php';
     include_once 'includes/footer.php';
