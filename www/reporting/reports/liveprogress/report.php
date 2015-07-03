97b666674094f697d5afa4cfa819dda2db0d44bb
Michael Sharman <sharmo@gmail.com>
2015-07-03 13:36:08 +1000

    Updated event verbs and added events to log

--- www/reporting/reports/liveprogress/report.php
+++ www/reporting/reports/liveprogress/report.php
@@ -58,29 +58,29 @@ $signedRequest = $Init->generate();
         reportsApp = LearnosityReports.init(initOptions, eventOptions);
 
     function init () {
-        reportsApp.getReport('report-1').on('started', function (events) {
-            console.log('Received events: started');
+        reportsApp.getReport('report-1').on('start', function (events) {
+            console.log('Received events: start', events);
         });
         reportsApp.getReport('report-1').on('paused', function (events) {
-            console.log('Received events: paused');
+            console.log('Received events: paused', events);
         });
         reportsApp.getReport('report-1').on('resumed', function (events) {
-            console.log('Received events: resumed');
+            console.log('Received events: resumed', events);
         });
         reportsApp.getReport('report-1').on('saved', function (events) {
-            console.log('Received events: saved');
+            console.log('Received events: saved', events);
         });
-        reportsApp.getReport('report-1').on('submitted', function (events) {
-            console.log('Received events: submitted');
+        reportsApp.getReport('report-1').on('submit', function (events) {
+            console.log('Received events: submit', events);
         });
         reportsApp.getReport('report-1').on('progressed', function (events) {
-            console.log('Received events: progressed');
+            console.log('Received events: progressed', events);
         });
         reportsApp.getReport('report-1').on('consumed', function (events) {
-            console.log('Received events: consumed');
+            console.log('Received events: consumed', events);
         });
-        reportsApp.getReport('report-1').on('terminated', function (events) {
-            console.log('Received events: terminated');
+        reportsApp.getReport('report-1').on('terminate', function (events) {
+            console.log('Received events: terminate', events);
         });
 
     }
