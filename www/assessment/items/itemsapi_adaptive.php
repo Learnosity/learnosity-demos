35f62adb14eae6fefced5311386cad9874c1764a
gonzalozawa <gonzalo.zawadzki@learnosity.com>
2015-08-25 13:03:58 +0100

    Fixing adaptive branching docs links

--- www/assessment/items/itemsapi_adaptive.php
+++ www/assessment/items/itemsapi_adaptive.php
@@ -94,7 +94,7 @@ $signedRequest = $Init->generate();
         <ul class="list-inline">
             <li data-toggle="tooltip" data-original-title="Customise API Settings"><a href="#" class="text-muted" data-toggle="modal" data-target="#settings"><span class="glyphicon glyphicon-list-alt"></span></a></li>
             <li data-toggle="tooltip" data-original-title="Preview API Initialisation Object"><a href="#"  data-toggle="modal" data-target="#initialisation-preview"><span class="glyphicon glyphicon-search"></span></a></li>
-            <li data-toggle="tooltip" data-original-title="Visit the documentation"><a href="http://docs.learnosity.com/itemsapi/adaptive/itemadaptive.php" title="Documentation"><span class="glyphicon glyphicon-book"></span></a></li>
+            <li data-toggle="tooltip" data-original-title="Visit the documentation"><a href="http://docs.learnosity.com/assessment/items/knowledgebase/adaptiveassessment" title="Documentation"><span class="glyphicon glyphicon-book"></span></a></li>
         </ul>
     </div>
     <h1>Items API – Adaptive Assessment</h1>
