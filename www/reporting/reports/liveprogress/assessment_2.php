752197f43191705ccb6b5bfca817cbe57ca660aa
Michael Sharman <sharmo@gmail.com>
2015-08-26 15:02:00 +1000

    Turned on question count option

--- www/reporting/reports/liveprogress/assessment_2.php
+++ www/reporting/reports/liveprogress/assessment_2.php
@@ -32,7 +32,10 @@ $request = array(
             'pwd' => '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8' // `password`
         ),
         'navigation' => array(
-            'intro_item' => 'demo-intro-live-progress'
+            'intro_item' => 'demo-intro-live-progress',
+            'item_count' => array(
+               'question_count_option' => true
+            )
         ),
         'regions' => array(
             'top-right' => array(
