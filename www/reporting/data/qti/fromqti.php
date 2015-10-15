51b9260606018df672d79badf775487cb1a9d80d
Stella Lie <ask@stellalie.com>
2015-10-16 10:01:10 +1100

    [BUG] Move raw xml inside php file and escape its content

--- www/reporting/data/qti/fromqti.php
+++ www/reporting/data/qti/fromqti.php
@@ -2,6 +2,7 @@
 
 $endpoint = "$URL/$version/itembank/conversion/fromqti";
 $resource = 'fromqti';
+$sampleXml = htmlspecialchars(file_get_contents(__DIR__ . '/sample.xml'));
 
 ?>
 
@@ -22,36 +23,7 @@ $resource = 'fromqti';
             <div class="form-group">
                 <label class="col-md-2 control-label">QTI</label>
                 <div class="col-md-10">
-                    <textarea class="form-control" id="api-items" data-type="array_single"><?xml version="1.0" encoding="UTF-8"?>
-<assessmentItem xmlns="http://www.imsglobal.org/xsd/imsqti_v2p1"
-    xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-    xsi:schemaLocation="http://www.imsglobal.org/xsd/imsqti_v2p1  http://www.imsglobal.org/xsd/qti/qtiv2p1/imsqti_v2p1.xsd"
-    identifier="choice" title="Unattended Luggage" adaptive="false" timeDependent="false">
-    <responseDeclaration identifier="RESPONSE" cardinality="single" baseType="identifier">
-        <correctResponse>
-            <value>ChoiceA</value>
-        </correctResponse>
-    </responseDeclaration>
-    <outcomeDeclaration identifier="SCORE" cardinality="single" baseType="float">
-        <defaultValue>
-            <value>0</value>
-        </defaultValue>
-    </outcomeDeclaration>
-    <itemBody>
-        <p>Look at the text in the picture.</p>
-        <p>
-            <img src="images/sign.png" alt="NEVER LEAVE LUGGAGE UNATTENDED"/>
-        </p>
-        <choiceInteraction responseIdentifier="RESPONSE" shuffle="false" maxChoices="1">
-            <prompt>What does it say?</prompt>
-            <simpleChoice identifier="ChoiceA">You must stay with your luggage at all times.</simpleChoice>
-            <simpleChoice identifier="ChoiceB">Do not let someone else look after your luggage.</simpleChoice>
-            <simpleChoice identifier="ChoiceC">Remember your luggage when you leave.</simpleChoice>
-        </choiceInteraction>
-    </itemBody>
-    <responseProcessing
-        template="http://www.imsglobal.org/question/qti_v2p1/rptemplates/match_correct"/>
-</assessmentItem></textarea>
+                    <textarea class="form-control" id="api-items" data-type="array_single"><?php echo $sampleXml ?></textarea>
                 </div>
             </div>
             <div class="form-group">
