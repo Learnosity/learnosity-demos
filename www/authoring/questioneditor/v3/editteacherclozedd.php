5790bd76dcaf272a9a819a8e47fccba853fee036
Mark Lynch <mark@learnosity.com>
2016-10-14 08:33:22 +1100

    [FIX] Remove distractors

--- www/authoring/questioneditor/v3/editteacherclozedd.php
+++ www/authoring/questioneditor/v3/editteacherclozedd.php
@@ -68,7 +68,8 @@ include_once 'includes/header.php';
     <span data-lrn-qe-label="possible_responses"></span>
     <div data-lrn-qe-loop="possible_responses[*]">
         <span data-lrn-qe-input="possible_responses[*]"></span>
-        <span data-lrn-qe-input="metadata.distractor_rationale_response_level[*]"></div>
+        <!-- Use for interleaving distractors
+        <span data-lrn-qe-input="metadata.distractor_rationale_response_level[*]"></div -->>
     </div>
     <span data-lrn-qe-action-add="possible_responses"></span>
 
