b4f6eff58453db0877570878ec6428c127dd3106
Olivier Mehani <olivier.mehani@learnosity.com>
2017-04-24 10:57:23 +1000

    [DOC] Add comment about where to find actual liveprogress demo code
    The link from the page would lead to the mostly uninteresting iframe
    page, so we help the reader to find the real meat of the demo.
    
    Signed-off-by: Olivier Mehani <olivier.mehani@learnosity.com>

--- www/analytics/reports/live_progress.php
+++ www/analytics/reports/live_progress.php
@@ -7,6 +7,26 @@ use LearnositySdk\Utils\Uuid;
 
 $studentIds = array(Uuid::generate(), Uuid::generate(), Uuid::generate(), Uuid::generate());
 
+/*
+ * Hey there, demos visitor!
+ *
+ * You might have reached this page via the View source link on the demos site, an are know a bit bemused as this was
+ * not the code you were looking for.
+ *
+ * For ease of demonstration, the root of the live progress demos (this file) only shows 4 iframes containing the
+ * report, and 3 assessements. The source for the iframes is whas you are looking for.
+ *
+ * Fear not, you can easily reach them. As you can see below, they are in the `liveprogress` subdirectory, and are named
+ * `report.php`, and `assessment_n.php`, respectively. All assessments pages look the same, and actually include a
+ * common file, `assessment.inc.php`, which is probably what you'd be most inretested in.
+ *
+ * Assuming you are viewing this file in Github, you can easily access those files by hitting the `t` key now, which
+ * will bring up a file-search form. You can then just start typing the name of the file you're after, and hit `Enter`
+ * when it is shown. As mentioned before, the most interesting files are
+ * - www/analytics/reports/liveprogress/report.php (e.g., type `livproreport` in the search form),
+ * - www/analytics/reports/liveprogress/assessment.inc.php (e.g., type `liveproassessinc` in the search form).
+ */
+
 ?>
 
 <!-- Basic styles to remove padding from the main layout -->
