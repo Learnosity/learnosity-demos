038377eee976efdeacd9224f536ec2cf4a84eaa4
Mark Lynch <mark@learnosity.com>
2017-06-06 14:46:08 +1000

    [CLEANUP] Remove $url_authorapi_legacy

--- www/config.php
+++ www/config.php
@@ -88,7 +88,6 @@ $curl_ssl_verify = true;
 
 $url_assess                = '//assess-va.learnosity.com';
 $url_authorapi             = '//authorapi.learnosity.com?v1';
-$url_authorapi_legacy      = '//authorapi.learnosity.com?v0.5';
 $url_data                  = 'https://data-va.learnosity.com';
 $url_events                = '//events-va.learnosity.com';
 $url_items                 = '//items-va.learnosity.com';
