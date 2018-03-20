6e92c0896c51cd9a87a4ed1b1e899c8a9b00150a
Michael Sharman <sharmo@gmail.com>
2018-03-21 06:19:28 +1100

    Added missing adminstration panel options

--- www/assessment/items/itemsapi_assess.php
+++ www/assessment/items/itemsapi_assess.php
@@ -155,6 +155,15 @@ $request = [
             'beta_flags' => [
                 'reactive_views' => true
             ]
+        ],
+        'administration' => [
+            'pwd' => '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8',
+            'options' => [
+                'show_save' => true,
+                'show_submit' => true,
+                'show_exit' => true,
+                'show_extend' => true
+            ]
         ]
     ]
 ];
