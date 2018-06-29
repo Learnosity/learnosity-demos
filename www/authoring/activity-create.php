5a85d9051dd787b9954d74992b023985d8a9a49b
Conor Walsh <conor.walsh@learnosity.com>
2018-06-29 17:54:04 +0100

    [FEATURE] Add Activity Create Demo

---
+++ www/authoring/activity-create.php
@@ -0,0 +1,84 @@
+<?php
+
+//common environment attributes including search paths. not specific to Learnosity
+include_once '../env_config.php';
+
+//site scaffolding
+include_once 'includes/header.php';
+
+//common Learnosity config elements including API version control vars
+include_once '../lrn_config.php';
+
+use LearnositySdk\Request\Init;
+use LearnositySdk\Utils\Uuid;
+
+$security = [
+    'consumer_key' => $consumer_key,
+    'domain'       => $domain
+];
+
+
+$request = [
+    'mode'      => 'activity_edit',
+    'reference' => Uuid::generate(),
+    'config'    => [
+        'activity_edit' => [
+            'mode' => [
+                'show' => true,
+                'default' => 'edit'
+            ]
+         ]
+    ],
+    'user' => [
+        'id'        => 'demos-site',
+        'firstname' => 'Demos',
+        'lastname'  => 'User',
+        'email'     => 'demos@learnosity.com'
+    ]
+];
+
+$Init = new Init('author', $security, $consumer_secret, $request);
+$signedRequest = $Init->generate();
+
+?>
+
+    <div class="jumbotron section">
+        <div class="toolbar">
+            <ul class="list-inline">
+                <li data-toggle="tooltip" data-original-title="Preview API Initialisation Object"><a href="#"  data-toggle="modal" data-target="#initialisation-preview"><span class="glyphicon glyphicon-search"></span></a></li>
+                <li data-toggle="tooltip" data-original-title="Visit the documentation"><a href="http://docs.learnosity.com/authorapi/" title="Documentation"><span class="glyphicon glyphicon-book"></span></a></li>
+            </ul>
+        </div>
+        <div class="overview">
+            <h2>Creating a New Activity / Test (using Author API)</h2>
+            <p>
+                The activity edit mode allows authors to create and edit actvities.
+            </p>
+        </div>
+    </div>
+
+    <div class="section pad-sml">
+        <!-- Container for the author api to load into -->
+        <div id="learnosity-author"></div>
+    </div>
+
+    <script src="<?php echo $url_authorapi; ?>"></script>
+    <script>
+        var initializationObject = <?php echo $signedRequest; ?>;
+
+        //optional callbacks for ready
+        var callbacks = {
+            readyListener: function () {
+                console.log("Author API has successfully initialized.");
+            },
+            errorListener: function (err) {
+                console.log(err);
+            }
+        };
+
+        var authorApp = LearnosityAuthor.init(initializationObject, callbacks);
+    </script>
+
+<?php
+include_once 'views/modals/initialisation-preview.php';
+include_once 'includes/footer.php';
