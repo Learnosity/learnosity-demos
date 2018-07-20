14a44a1f8eeec94d144ba5a008ad3be5a2cc45b8
Edwin Dawson <edwin.dawson@learnosity.com>
2018-07-20 17:01:28 +1000

    [DOC] Fix links to documentation.

--- www/analytics/data/index.php
+++ www/analytics/data/index.php
@@ -18,7 +18,7 @@ $version = $lts_version;
 <div class="jumbotron section">
     <div class="toolbar">
         <ul class="list-inline">
-            <li data-toggle="tooltip" data-original-title="Visit the documentation"><a href="http://docs.learnosity.com/dataapi/" title="Documentation"><span class="glyphicon glyphicon-book"></span></a></li>
+            <li data-toggle="tooltip" data-original-title="Visit the documentation"><a href="https://docs.learnosity.com/analytics/data" title="Documentation"><span class="glyphicon glyphicon-book"></span></a></li>
             <li data-toggle="tooltip" data-original-title="Toggle product overview box"><a href="#"><span class="glyphicon glyphicon-chevron-up jumbotron-toggle"></span></a></li>
         </ul>
     </div>
