f0320d7fba80b57e47ffdb0f93edcd057020eda2
hbalseiro <38862728+hbalseiro@users.noreply.github.com>
2018-07-30 17:19:39 -0400

    Update live_progress.php
    Fixed typos

--- www/analytics/reports/live_progress.php
+++ www/analytics/reports/live_progress.php
@@ -18,12 +18,12 @@ $studentIds = array(Uuid::generate(), Uuid::generate(), Uuid::generate(), Uuid::
  *
  * Fear not, you can easily reach them. As you can see below, they are in the `liveprogress` subdirectory, and are named
  * `report.php`, and `assessment_n.php`, respectively. All assessments pages look the same, and actually include a
- * common file, `assessment.inc.php`, which is probably what you'd be most inretested in.
+ * common file, `assessment.inc.php`, which is probably what you'd be most interested in.
  *
  * Assuming you are viewing this file in Github, you can easily access those files by hitting the `t` key now, which
  * will bring up a file-search form. You can then just start typing the name of the file you're after, and hit `Enter`
  * when it is shown. As mentioned before, the most interesting files are
- * - www/analytics/reports/liveprogress/report.php (e.g., type `livproreport` in the search form),
+ * - www/analytics/reports/liveprogress/report.php (e.g., type `liveproreport` in the search form),
  * - www/analytics/reports/liveprogress/assessment.inc.php (e.g., type `liveproassessinc` in the search form).
  */
 
