291f6a1655df2229c5bd24df1618de0e129446e8
Neil McGough <neil.mcgough@learnosity.com>
2018-10-25 12:51:38 -0400

    added updated analytics homepage

--- www/analytics/index.php
+++ www/analytics/index.php
@@ -19,18 +19,60 @@ include_once 'includes/header.php';
     <h4><span class="badge btn-warning">Note</span> Placeholder landing page. Titles most of the way there; blurbs and links in progress</h4>
     <p>&nbsp;</p> <!--replace with CSS-->
 
-    <h3>Reports API</h3>
-    <p>&nbsp;</p> <!--replace with CSS-->
-
+    <h3>Embedded dashboard-style Reporting</h3>
+    <p>Easily use Learnosity's Reports API to embed visual reports, ranging from student & classroom reporting to large scale district reports!</p>
+    <br>
 
     <div class="row">
         <div class="col-md-6">
             <div class="panel panel-default">
                 <div class="panel-heading">
-                    <h2 class="panel-title">No UI (Raw data only)</h2>
+                    <h2 class="panel-title">Student-Centric reporting</h2>
                 </div>
                 <div class="panel-body">
-                    <p>Gain access to the raw data from the Learnosity platform by turning off the report rendering (ui).</p>
+                    <p>Learn more about individual students in an easy, in-depth fashion!</p>
+                    <p class="text-right">
+                        <a class="btn btn-primary btn-md" href="./reportsapi_no_ui.php">Demo</a>
+                    </p>
+                </div>
+            </div>
+        </div>
+            <div class="col-md-6">
+
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h2 class="panel-title">Classroom & Teacher-Centric reporting</h2>
+                </div>
+                <div class="panel-body">
+                    <p>Monitor and compare the progress of your students at a glance.</p>
+                    <p class="text-right">
+                        <a class="btn btn-primary btn-md" href="./reportsapi_no_ui.php">Demo</a>
+                    </p>
+                </div>
+            </div>
+            </div>
+            <div class="col-md-6">
+
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h2 class="panel-title">Coursework Progress reporting</h2>
+                </div>
+                <div class="panel-body">
+                    <p>Learn more about individual students in an easy, in-depth fashion!</p>
+                    <p class="text-right">
+                        <a class="btn btn-primary btn-md" href="./reportsapi_no_ui.php">Demo</a>
+                    </p>
+                </div>
+            </div>
+            </div>
+            <div class="col-md-6">
+
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h2 class="panel-title">Large-Scale cohort / district reporting</h2>
+                </div>
+                <div class="panel-body">
+                    <p>Learn more about individual students in an easy, in-depth fashion!</p>
                     <p class="text-right">
                         <a class="btn btn-primary btn-md" href="./reportsapi_no_ui.php">Demo</a>
                     </p>
@@ -40,12 +82,79 @@ include_once 'includes/header.php';
         <div class="col-md-6">
             <div class="panel panel-default">
                 <div class="panel-heading">
-                    <h2 class="panel-title">Live Progress Tracking</h2>
+                    <h2 class="panel-title">Live Progress Tracking & Control</h2>
                 </div>
                 <div class="panel-body">
-                    <p>An interactive demo, simulating 3 students taking a test with an administrator viewing their progress in real time. Also shows the power of control events, which can be used to remote control an end user's assessment in real time.</p>
+                    <p></p>
                     <p class="text-right">
                         <a class="btn btn-primary btn-md" href="./reportsapi_live_progress.php">Demo</a>
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h2 class="panel-title">Learning Outcomes & Mastery</h2>
+                </div>
+                <div class="panel-body">
+                    <p></p>
+                    <p class="text-right">
+                        <a class="btn btn-primary btn-md" href="./reportsapi_live_progress.php">Demo</a>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <h3>Customizing your reporting experience</h3>
+    <p></p>
+    <br>
+
+    <div class="row">
+        <div class="col-md-6">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h2 class="panel-title">Disabling Reporting UI</h2>
+                </div>
+                <div class="panel-body">
+                    <p>In some cases, you may want to use our in-browser reporting widget info, but your own skin and styling on top. Find out more about getting just the report info, and how you can use it!</p>
+                    <p class="text-right">
+                        <a class="btn btn-primary btn-md" href="./reportsapi_no_ui.php">Demo</a>
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6">
+
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h2 class="panel-title">Using click events</h2>
+                </div>
+                <div class="panel-body">
+                    <p>Use click events in our visual </p>
+                    <p class="text-right">
+                        <a class="btn btn-primary btn-md" href="./reportsapi_no_ui.php">Demo</a>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <h3>Granular, back-end data reporting</h3>
+    <p></p>
+    <br>
+
+    <div class="row">
+        <div class="col-md-6">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h2 class="panel-title">Data API Examples</h2>
+                </div>
+                <div class="panel-body">
+                    <p>Sometimes, you just need access to all the raw info you can handle! Using Learnosity's Data API, you can </p>
+                    <p class="text-right">
+                        <a class="btn btn-primary btn-md" href="./reportsapi_no_ui.php">Demo</a>
                     </p>
                 </div>
             </div>
