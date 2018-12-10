b8589bc410566b99337c732f3fa623a1a5e6288c
Conor Walsh <conor.walsh@learnosity.com>
2018-12-10 18:03:41 +0000

    [CLEANUP] Update demo footer style

--- src/includes/footer.php
+++ src/includes/footer.php
@@ -4,35 +4,21 @@
     <div class="section">
         <div class="row">
             <div class="col-lg-3 col-md-12">
-                <a href="<?php echo $baseWebPath; ?>" class="logo">
+                <a href="<?php echo $baseWebPath; ?>">
                     <img src="/static/images/lrn-demos-logo-white-2x.png" alt="Learnosity Demos" class="logo">
                 </a>
             </div>
-            <div class="col-md-12 col-lg-9">
-                <div class="row">
-                    <div class="col-md-2">
-                        <h4><a href="https://help.learnosity.com">Developer Help</a></h4>
-                    </div>
-                    <div class="col-md-2">
-                        <h4><a href="https://authorguide.learnosity.com">Author Guide</h4>
-                    </div>
-                    <div class="col-md-2">
-                        <h4><a href="https://reference.learnosity.com">API Reference</h4>
-                    </div>
-                    <div class="col-md-2">
-                        <h4><a href="https://status.learnosity.com">API Status</h4>
-                    </div>
-                    <div class="col-md-2">
-                        <h4><a href="https://demos.learnosity.com">Demos</h4>
-                    </div>
-                    <div class="col-md-2">
-                        <h4><a href="https://learnosity.com">Visit Learnosity</h4>
-                    </div>
-                </div>
+            <div class="col-lg-9 col-md-12 footer-links">
+                <a href="https://help.learnosity.com">Developer Help</a>
+                <a href="https://authorguide.learnosity.com">Author Guide</a>
+                <a href="https://reference.learnosity.com">API Reference</a>
+                <a href="https://status.learnosity.com">API Status</a>
+                <a href="https://demos.learnosity.com">Demos</a>
+                <a href="https://learnosity.com">Visit Learnosity</a>
             </div>
-
         </div>
     </div>
 </div>
+
 </body>
 </html>
