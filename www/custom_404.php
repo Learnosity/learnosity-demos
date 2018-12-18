d11f6ce86f3653fd30e5a5e383321036d39a763f
Neil McGough <neil.mcgough@learnosity.com>
2018-12-18 14:51:30 +0000

    added custom 404

---
+++ www/custom_404.php
@@ -0,0 +1,26 @@
+<?php
+include_once 'env_config.php';
+include_once 'includes/header.php';
+?>
+
+<div class="landing section index">
+    <div class="jumbotron">
+        <div class="landing-intro">
+            <div class="media">
+                <div class="media-body">
+                    <h1>Uh oh!</h1>
+                    <h2>404 Not Found</h2>
+                    </p><p>
+                        Unfortunately, we can't find the page you're looking for. We'd suggest starting over from the <a href="/">home page.</a></p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    </div>
+
+
+</div>
+
+<?php
+    include_once 'includes/footer.php';
