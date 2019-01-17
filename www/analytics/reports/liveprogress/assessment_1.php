6a79303bf5008aa80fbbbc75dc1ba1ed580b70c7
gonzalozawa <gonzalo.zawadzki@learnosity.com>
2019-01-17 17:51:47 +0000

    Cleanup legacy demos

--- www/analytics/reports/liveprogress/assessment_1.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$student = array(
-    'id'   => htmlspecialchars($_GET['user_id'], ENT_QUOTES),
-    'name' => 'Jesse Pinkman'
-);
-
-include_once 'assessment.inc.php';
--- www/analytics/reports/liveprogress/assessment_4.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$student = array(
-    'id'   => htmlspecialchars($_GET['user_id'], ENT_QUOTES),
-    'name' => 'Mike Ehrmantraut'
-);
-
-include_once 'assessment.inc.php';
