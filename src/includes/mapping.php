090d8aa011fd8f582e9d13fbfbb237e8020e2969
Olivier Mehani <olivier.mehani@learnosity.com>
2019-05-22 17:09:24 +1000

    fixup! fixup! [FEATURE] Add mapping for redirects

--- src/includes/mapping.php
+++ src/includes/mapping.php
@@ -2,8 +2,9 @@
 
 $mapping = [
     /* Note: order in this array matters, as the first rule that matches will be unconditionally applied */
-    '/analytics/reports/live_progress.php' =>'/analytics/live-progress-reporting.php',
-    '/analytics/reports/liveprogress' =>'/analytics/liveprogress',
+    '/analytics/reports/live_progress.php' => '/analytics/live-progress-reporting.php',
+    '/analytics/reports/liveprogress' => '/analytics/liveprogress',
+    '/assessment/items/itemsapi_itemadaptive.php' => '/assessment/adaptive.php',
 ];
 
 // add redirect routes for old urls
@@ -12,8 +13,8 @@ foreach ($mapping as $oldURL => $newURL) {
         $fullNewURL = str_replace($oldURL, $newURL, $_SERVER['REQUEST_URI']);
         http_response_code(302);
         header('Location: ' . $fullNewURL);
-        /* In case we cannot use Location, e.g., been called from a 404 handler
-         * which doesn't allow us to override the status ) */
+        /* In case we cannot use Location, e.g., we've been called from a 404 handler
+         * which doesn't allow us to override the status) */
         print(
             "<!doctype html>" . PHP_EOL
             . "<html>" . PHP_EOL
