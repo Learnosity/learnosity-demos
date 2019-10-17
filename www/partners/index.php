5b36464ec64ae79255186e41740c637af535c0af
Angelique Dubois <55494026+angelique-dubois-lrn@users.noreply.github.com>
2019-10-17 17:10:44 +0100

    Adds Partners Section
    * Add Partners to Nav Menu
    
    * Move TextHelp demo to Partners section
    
    * Minor CSS change in partners/texthelp.php
    
    * Minor update to partners/texthelp.php
    
    * remove image file

---
+++ www/partners/index.php
@@ -0,0 +1,40 @@
+<?php
+
+include_once '../env_config.php';
+include_once 'includes/header.php';
+?>
+
+    <div class="jumbotron section index">
+        <h1>Partners</h1>
+
+        <div class="section-intro">
+            <p>
+            Learnosity’s partners are some of the world’s most innovative edtech companies. 
+            <br/>Collaborating in developing, scaling, and delivering exceptional learning products.
+            </p>
+            <ul>
+                <li><h4><a class="blue-chevron" href="#texthelp">TextHelp</a></h4></li>
+            </ul>
+            </p>
+        </div>
+
+        <div class="row">
+            <div class="col-md-6">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h2 class="panel-title" id="texthelp">Using Third-Party Assistive Tools: TextHelp</h2>
+                    </div>
+                    <div class="panel-body">
+                        <p>This demonstrates integrating Learnosity with TextHelp. TextHelp's SpeechStream is a cloud-based 
+                        JavaScript software solution that allows publishers to embed text-to-speech into their assessment items.</p>
+                        <p class="text-right">
+                            <a class="demo_link" href="./texthelp.php">Demo</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+
+<?php include_once 'includes/footer.php';
--- www/partners/texthelp.php
+++ www/partners/texthelp.php
@@ -30,9 +30,25 @@ $request = [
         'configuration' => [
             'onsubmit_redirect_url' => 'texthelp.php'
         ],
-        'questions_api_init_options' => [
-            'beta_flags' => [
-                'reactive_views' => true
+        'region_overrides' => [
+            'right' => [
+                [
+                    'type' => 'save_button'
+                ],
+                [
+                    'type' => 'fullscreen_button'
+                ],
+                [
+                    'type' => 'accessibility_button'
+                ],
+                [
+                    'type' => 'custom_button',
+                    'options' => [
+                        'name' => 'SpeechStream',
+                        'label' => 'SpeechStream',
+                        'icon_class' => 'lrn_btn SpeechStream_btn'
+                    ]
+                ]
             ]
         ]
     ]
@@ -81,10 +97,13 @@ $signedRequest = $Init->generate();
             console.log("Listener fired");
             var assessApp = itemsApp.assessApp();
             assessApp.on('test:start', function() {
-                // When the assessment starts we find the elements within the assessment wrapper
-                // that we want the Texthelp reader to ignore and add the 'ignore' attribute to them.
-                // Initiate Texthelp only when the Learnoisty assessment starts
-                TexthelpSpeechStream.addToolbar('1','1');
+                // When the assessment starts, Texthelp’s SpeechStream will parse through the DOM 
+                // and dynamically ignore certain specified ‘classes’ that are listed in SpeechStream’s configuration file, 
+                // which were previously identified as not to be read aloud.
+                TexthelpSpeechStream.addToolbar();
+            });
+            assessApp.on('button:SpeechStream:clicked', function(){
+                showSpeechStreamBar();
             });
         }
     });
@@ -95,7 +114,36 @@ $signedRequest = $Init->generate();
         $rw_setStartPoint("start");
     }
 
+    var showSpeechStream = true;
+
+    function showSpeechStreamBar(){
+        showSpeechStream = !showSpeechStream;
+        window.texthelp.SpeechStream.ui.toolbar.toolbar.setVisibility(showSpeechStream);
+        $rw_stopSpeech();  
+        $rw_enableClickToSpeak(false);  
+    }
+
 </script>
+
+<style>
+    .lrn.lrn-assess .lrn-region:not(.lrn-items-region) .lrn_btn.SpeechStream_btn{
+        padding: 0.4em 0.9em 0.4em 0.7em;
+    }
+    @media all and (max-width: 991px) {
+        .lrn.lrn-assess .lrn-region:not(.lrn-items-region) .lrn_btn.SpeechStream_btn{
+            padding: 0.75em 0.25em; 
+        }
+    }
+    button.lrn_btn.SpeechStream_btn:before{
+        content: '';
+        background-image: url('../static/images/speechstream-logo.png');
+        background-size: contain;
+        float: left;
+        padding: 6px;
+        height: 26px;
+        width: 23px;
+    }
+</style>
 
 <?php
     include_once 'views/modals/initialisation-preview.php';
