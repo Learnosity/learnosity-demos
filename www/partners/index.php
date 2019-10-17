3f439048bb5984dc6f16d7350d5b29e24699a3c5
Angelique Dubois <55494026+angelique-dubois-lrn@users.noreply.github.com>
2019-10-17 17:52:58 +0100

    Add Desmos Demo
    * Add Desmos demo. Remove Doc links in Partners demos.

--- www/partners/index.php
+++ www/partners/index.php
@@ -14,8 +14,8 @@ include_once 'includes/header.php';
             </p>
             <ul>
                 <li><h4><a class="blue-chevron" href="#texthelp">TextHelp</a></h4></li>
+                <li><h4><a class="blue-chevron" href="#desmos">Desmos</a></h4></li>
             </ul>
-            </p>
         </div>
 
         <div class="row">
@@ -29,6 +29,20 @@ include_once 'includes/header.php';
                         JavaScript software solution that allows publishers to embed text-to-speech into their assessment items.</p>
                         <p class="text-right">
                             <a class="demo_link" href="./texthelp.php">Demo</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h2 class="panel-title" id="desmos">Helping every student love learning math: Desmos</h2>
+                    </div>
+                    <div class="panel-body">
+                        <p>Desmos create best-in-class digital math tools. Through the Desmos and Learnosity partnership, 
+                        Learnosity clients can leverage graphing and scientific calculators.</p>
+                        <p class="text-right">
+                            <a class="demo_link" href="./desmos.php">Demo</a>
                         </p>
                     </div>
                 </div>
