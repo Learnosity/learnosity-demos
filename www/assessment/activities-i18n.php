b393b0746e064ad4d2ea6caa6e56fd5dbb9f5082
Ed Dawson <edwin.dawson@gmail.com>
2019-11-04 14:06:02 +1100

    [BUG] Fix text spacing on links.

--- www/assessment/activities-i18n.php
+++ www/assessment/activities-i18n.php
@@ -188,9 +188,9 @@ $signedRequest = $Init->generate();
             <br/>
             <p>
                 <b>More languages:</b>
-                <a class="other-language <?php if ($language === 'en-GB') { echo 'selected'; } ?>" href="/assessment/activities-i18n.php?language=en-GB">English / UK</a>
-                 | <a class="other-language <?php if ($language === 'tl') { echo 'selected'; } ?>" href="/assessment/activities-i18n.php?language=tl">Filipino / Tagalog</a>
-                 | <a class="other-language <?php if ($language === 'it') { echo 'selected'; } ?>" href="/assessment/activities-i18n.php?language=it">Italiano / Italian</a>
+                <a class="other-language <?php if ($language === 'en-GB') { echo 'selected'; } ?>" href="/assessment/activities-i18n.php?language=en-GB">English/UK</a>
+                 | <a class="other-language <?php if ($language === 'tl') { echo 'selected'; } ?>" href="/assessment/activities-i18n.php?language=tl">Filipino/Tagalog</a>
+                 | <a class="other-language <?php if ($language === 'it') { echo 'selected'; } ?>" href="/assessment/activities-i18n.php?language=it">Italiano/Italian</a>
             </p>
             <p><b>Add your own:</b> Documentation | <a href="https://github.com/Learnosity/learnosity-i18n">Github repo</a> </p>
 
