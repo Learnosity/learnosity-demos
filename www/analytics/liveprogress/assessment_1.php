f6f42233da52a52a769cfd7d10dad3e8f00a103e
Horacio Balseiro <horacio.balseiro@learnosity.com>
2020-09-02 10:35:50 -0400

    Change erroneous INPUT_POST to INPUT_GET

--- www/analytics/liveprogress/assessment_1.php
+++ www/analytics/liveprogress/assessment_1.php
@@ -1,7 +1,7 @@
 <?php
 
 $student = [
-    'id'   => htmlspecialchars(filter_input(INPUT_POST, 'user_id', FILTER_SANITIZE_FULL_SPECIAL_CHARS), ENT_QUOTES),
+    'id'   => htmlspecialchars(filter_input(INPUT_GET, 'user_id', FILTER_SANITIZE_FULL_SPECIAL_CHARS), ENT_QUOTES),
     'name' => 'Jesse Pinkman'
 ];
 
