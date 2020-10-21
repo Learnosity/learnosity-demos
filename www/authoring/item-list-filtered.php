b28756b49e012fa64eae5f3d32cc29918f03f59a
Horacio Balseiro <horacio.balseiro@learnosity.com>
2020-10-21 14:40:53 -0400

    Add audio recording to simple features

--- www/authoring/item-list-filtered.php
+++ www/authoring/item-list-filtered.php
@@ -79,7 +79,8 @@
 					 */
 					'dynamic_content' => true,
 					'duplicate' => true,
-					'shared_passage' => true
+					'shared_passage' => true,
+ 	                'enable_audio_recording'=>true
 				]
 			]
 		],
