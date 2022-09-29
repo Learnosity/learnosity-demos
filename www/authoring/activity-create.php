069eca0b3d8a0ed920b80d9b9e663633187e7ff5
Edwin Dawson <edwin.dawson@learnosity.com>
2022-09-29 11:35:37 +1000

    [DOC] Fixup.

--- www/authoring/activity-create.php
+++ www/authoring/activity-create.php
@@ -46,7 +46,7 @@ $signedRequest = $Init->generate();
         <div class="overview">
             <h2>Create New Activities</h2>
             <p>
-                The activity edit mode allows authors to create and edit activities that are automatically saved to your Learnosity-hosted item bankz.
+                The activity edit mode allows authors to create and edit activities that are automatically saved to your Learnosity-hosted item bank.
             </p>
         </div>
     </div>
