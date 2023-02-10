3e908744449333ad909c05e093f11c61339ff836
Binny Kim <dabin.kim@learnosity.com>
2023-02-10 14:35:35 +1100

    [COPY] Update demo page copy and add link LRN-39111

--- www/assessment/reset-response.php
+++ www/assessment/reset-response.php
@@ -36,15 +36,15 @@ $requestDemo = [
 ];
 
 $InitDemo = new Init('items', $security, $consumer_secret, $requestDemo);
-$signedRequestDemo = $InitDemo->generate();
+$signedRequest = $InitDemo->generate();
 
 ?>
     <div class="jumbotron section">
         <div class="overview">
             <h2>Reset Response</h2>
             <br>
-            <p>Use the <a href="https://reference.learnosity.com/questions-api/methods#questionsApp-ResetResponse"><code>resetResponse</code></a> public method to allow user to reset the response of a question.</p>
-            <p>Below is a demo of the ‘resetResponse’(link to site reference) public method showing what can be achieved with a button that lets the user reset their response to a question. This can be useful in certain situations where end-users can be penalised for choosing an incorrect response. This public method provides a way for students to "reset" their response and leave a question unattempted if they’re not confident about the correct answer. This functionality is not available by default.</p>
+            <p>Use the <a href="https://reference.learnosity.com/questions-api/methods#question-ResetResponse"><code>resetResponse</code></a> public method to allow user to reset the response of a question.</p>
+            <p>Below is a demo of the <a href="https://reference.learnosity.com/questions-api/methods#question-ResetResponse"><code>resetResponse</code></a> public method showing what can be achieved with a button that lets the user reset their response to a question. This can be useful in certain situations where end-users can be penalised for choosing an incorrect response. This public method provides a way for students to "reset" their response and leave a question unattempted if they’re not confident about the correct answer. This functionality is not available by default.</p>
         </div>
     </div>
 
@@ -64,7 +64,7 @@ $signedRequestDemo = $InitDemo->generate();
         </div>
         <div class="button-note">
             <br>
-            <p>NOTE: The Reset Response button is a custom button that calls the <a href="https://reference.learnosity.com/questions-api/methods#questionsApp-ResetResponse"><code>resetResponse</code></a> public method.</p>
+            <p>NOTE: The Reset Response button is a custom button that calls the <a href="https://reference.learnosity.com/questions-api/methods#question-ResetResponse"><code>resetResponse</code></a> public method.</p>
         </div>
     </div>
 
@@ -84,7 +84,7 @@ $signedRequestDemo = $InitDemo->generate();
         </div>
         <div class="button-note">
             <br>
-            <p>NOTE: The Reset Response button is a custom button that calls the <a href="https://reference.learnosity.com/questions-api/methods#questionsApp-ResetResponse"><code>resetResponse</code></a> public method.</p>
+            <p>NOTE: The Reset Response button is a custom button that calls the <a href="https://reference.learnosity.com/questions-api/methods#question-ResetResponse"><code>resetResponse</code></a> public method.</p>
         </div>
     </div>
 
@@ -104,7 +104,7 @@ $signedRequestDemo = $InitDemo->generate();
         </div>
         <div class="button-note">
             <br>
-            <p>NOTE: The Reset Response button is a custom button that calls the <a href="https://reference.learnosity.com/questions-api/methods#questionsApp-ResetResponse"><code>resetResponse</code></a> public method.</p>
+            <p>NOTE: The Reset Response button is a custom button that calls the <a href="https://reference.learnosity.com/questions-api/methods#question-ResetResponse"><code>resetResponse</code></a> public method.</p>
         </div>
     </div>
 
@@ -138,7 +138,7 @@ $signedRequestDemo = $InitDemo->generate();
         }
 
         // Initialise Items API
-        const itemsApp = LearnosityItems.init(<?php echo $signedRequestDemo; ?>, callback);
+        const itemsApp = LearnosityItems.init(<?php echo $signedRequest; ?>, callback);
     </script>
 
     <style>
