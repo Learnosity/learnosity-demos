759b49953868cd857d335b71c7c5a378a1c2745a
aaron-ji-lrn <50093715+aaron-ji-lrn@users.noreply.github.com>
2023-07-14 16:38:19 +1000

    Revert "[FEATURE] add cut score demo page for last score by item by user LRN-…"

--- www/analytics/teacher-centric-reporting.php
+++ www/analytics/teacher-centric-reporting.php
@@ -34,6 +34,20 @@ $request = [
             ]
         ],
         [
+            'id' => 'lastscore-list-item',
+            'type' => 'lastscore-by-item-by-user',
+            'scoring_type' => 'partial',
+            "display_time_spent" => true,
+            "display_item_numbers" => true,
+            "activity_id" => 'Weekly_Math_Quiz',
+            'users' => [
+                ['id' => 'mce_student', 'name' => 'Jesse Pinkman'],
+                ['id' => 'mce_student_1', 'name' => 'Skylar White'],
+                ['id' => 'mce_student_2', 'name' => 'Walter White'],
+                ['id' => 'mce_student_3', 'name' => 'Saul Goodman']
+            ]
+        ],
+        [
             'id' => 'lastscore-activity-by-user',
             'type' => 'lastscore-by-activity-by-user',
             'scoring_type' => 'partial',
@@ -98,7 +112,7 @@ $Init = new Init('reports', $security, $consumer_secret, $request);
 $signedRequest = $Init->generate();
 
 ?>
-    <script src="<?php echo $url_reports; ?>"></script>
+
     <div class="jumbotron section">
         <div class="toolbar">
             <ul class="list-inline">
@@ -127,7 +141,10 @@ $signedRequest = $Init->generate();
     </div>
 
     <div class="section pad-sml">
-        <?php require 'last-score-by-item-by-user/index.php'; ?>
+        <!-- Container for the reports api to load into -->
+        <h3 id="lastscore-list-item-report"><a href="#lastscore-list-item-report">Last Score by Item by User Report</a></h3>
+        <p>Drill down and see exactly how each student did, per item. Helpful for identifying specific knowledge or understanding gaps in your group.</p>
+        <div id="lastscore-list-item"></div>
     </div>
 
     <div class="section pad-sml">
@@ -148,7 +165,7 @@ $signedRequest = $Init->generate();
         <div id="response-analysis"></div>
     </div>
 
-
+    <script src="<?php echo $url_reports; ?>"></script>
     <script>
 
         var reportsApp = LearnosityReports.init(<?php echo $signedRequest; ?>);
