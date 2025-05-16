0daa5ab287d8d5712d652392f323bf001b7535a0
Anthony  Murphy <anthony@mac.lan>
2025-05-16 11:36:45 +1000

    [FEATURE] Implement Cookiebot banner and remove legacy cookie banner

--- src/includes/header.php
+++ src/includes/header.php
@@ -6,7 +6,17 @@ if (!isset($pageTitle)) {
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="utf-8">
+    <script>
+        if (location.hostname.endsWith('.learnosity.com')) {
+            var cb = document.createElement('script');
+            cb.id = 'Cookiebot';
+            cb.src = 'https://consent.cookiebot.com/uc.js';
+            cb.setAttribute('data-cbid', '7d32057a-6a03-459f-84db-e232787c5485');
+            cb.setAttribute('data-blockingmode', 'auto');
+            cb.type = 'text/javascript';
+            document.head.appendChild(cb);
+        }
+    </script><meta charset="utf-8">
     <title><?= $pageTitle; ?></title>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -17,21 +27,19 @@ if (!isset($pageTitle)) {
 <body>
 
 <script>
-
-  const cookieBanner = new CookieBanner();
-  const cookiePreference = cookieBanner.getCookiePreference();
-
-  // If cookiePreference is set to true or is undefined, load the GA Tag Manager
-  if (cookiePreference === 'true' || cookiePreference === undefined) {
-
-    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','GTM-M2HXW6');
-
-  }
-
+if (window?.Cookiebot?.consented &&
+    location.hostname.endsWith('.learnosity.com')) {
+    (function(w,d,s,l,i){
+        w[l] = w[l] || [];
+        w[l].push({'gtm.start': new Date().getTime(), event: 'gtm.js'});
+        var f = d.getElementsByTagName(s)[0],
+            j = d.createElement(s),
+            dl = l !== 'dataLayer' ? '&l=' + l : '';
+        j.async = true;
+        j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+        f.parentNode.insertBefore(j, f);
+    })(window, document, 'script', 'dataLayer', 'GTM-M2HXW6');
+}
 </script>
 
 <?php
