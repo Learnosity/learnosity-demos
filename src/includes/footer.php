bc27b9c386b0c1531b6633d73ad4a7c0ae6265a7
Kerem Daldal <kerem.daldal@learnosity.com>
2025-09-03 22:02:07 +1000

    [BUG] Fix footer not sticking to bottom of page
    LRN-47944

--- src/includes/footer.php
+++ src/includes/footer.php
@@ -1,23 +1,24 @@
-    </div>
-</div>
-<div class="footer" role="contentinfo">
-    <div class="section">
-        <div class="row">
-            <div class="col-lg-3 col-md-12">
-                <a href="/">
-                    <img src="/static/images/lrn-demos-logo-white-2x.png" alt="Learnosity Demos" class="logo">
-                </a>
-            </div>
-            <div class="col-lg-9 col-md-12 footer-links">
-                <a href="https://support.learnosity.com/hc/en-us">Developer Help</a>
-                <a href="https://authorguide.learnosity.com/hc/en-us">Author Guide</a>
-                <a href="https://help.learnosity.com/hc/en-us/categories/16266193425053-Developer-Documentation">API Reference</a>
-                <a href="https://status.learnosity.com">API Status</a>
-                <a href="https://learnosity.com">Visit Learnosity</a>
-                <a href="/cookie-policy/index.php">Cookie Policy</a>
+        </div>
+    </main>
+    <footer class="footer" role="contentinfo">
+        <div class="section">
+            <div class="row">
+                <div class="col-lg-3 col-md-12">
+                    <a href="/">
+                        <img src="/static/images/lrn-demos-logo-white-2x.png" alt="Learnosity Demos" class="logo">
+                    </a>
+                </div>
+                <div class="col-lg-9 col-md-12 footer-links">
+                    <a href="https://support.learnosity.com/hc/en-us">Developer Help</a>
+                    <a href="https://authorguide.learnosity.com/hc/en-us">Author Guide</a>
+                    <a href="https://help.learnosity.com/hc/en-us/categories/16266193425053-Developer-Documentation">API Reference</a>
+                    <a href="https://status.learnosity.com">API Status</a>
+                    <a href="https://learnosity.com">Visit Learnosity</a>
+                    <a href="/cookie-policy/index.php">Cookie Policy</a>
+                </div>
             </div>
         </div>
-    </div>
+    </footer>
 </div>
 
 </body>
