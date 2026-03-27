262ecc74b0eb9dca3d6f6033be2de67df228890f
Horacio Balseiro <horacio.balseiro@learnosity.com>
2026-03-27 17:34:37 -0400

    [MAINTENANCE] Put authoring demos in maintenance mode

---
+++ www/authoring/question-edit.php
@@ -0,0 +1,67 @@
+<?php
+
+//common environment attributes including search paths. not specific to Learnosity
+include_once '../env_config.php';
+
+//site scaffolding
+include_once 'includes/header.php';
+
+//common Learnosity config elements including API version control vars
+include_once '../lrn_config.php';
+
+//alias(es) to eliminate the need for fully qualified classname(s) from sdk
+use LearnositySdk\Request\Init;
+
+
+//security object. timestamp added by SDK
+$security = [
+    'consumer_key' => $consumer_key,
+    'domain' => $domain
+];
+
+
+//simple api request object for item list view
+$request = [
+    'mode' => 'item_list',
+    'config' => [
+        'item_edit' => [
+            'item' => [
+                'reference' => [
+                    'show' => true,
+                    'edit' => true
+                ],
+                'dynamic_content' => true,
+                'shared_passage' => true,
+                'enable_audio_recording' => true
+            ]
+        ]
+    ],
+    'user' => [
+        'id' => 'demos-site',
+        'firstname' => 'Demos',
+        'lastname' => 'User',
+        'email' => 'demos@learnosity.com'
+    ]
+];
+
+$Init = new Init('author', $security, $consumer_secret, $request);
+$signedRequest = $Init->generate();
+
+?>
+
+    <div class="jumbotron section">
+        <div class="toolbar">
+            <ul class="list-inline">
+                <li data-toggle="tooltip" data-original-title="Preview API Initialisation Object"><a href="#"  data-toggle="modal" data-target="#initialisation-preview" aria-label="Preview API Initialisation Object"><span class="glyphicon glyphicon-search"></span></a></li>
+                <li data-toggle="tooltip" data-original-title="Visit the documentation"><a href="https://support.learnosity.com/hc/en-us/categories/360000105358-Learnosity-Author" title="Documentation"><span class="glyphicon glyphicon-book"></span></a></li>
+            </ul>
+        </div>
+        <div class="overview">
+        <h2>Maintenance Mode</h2>
+            <p>The Authoring Demos are currently undergoing maintenance and will return soon.</p>
+        </div>
+    </div>
+
+<?php
+include_once 'views/modals/initialisation-preview.php';
+include_once 'includes/footer.php';
