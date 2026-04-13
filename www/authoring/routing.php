16c20c5522526248f0550e75e2c0d68525143c15
Horacio Balseiro <horacio.balseiro@learnosity.com>
2026-04-13 09:27:14 -0400

    Revert "[MAINTENANCE] Put authoring demos in maintenance mode"
    This reverts commit 262ecc74b0eb9dca3d6f6033be2de67df228890f.
    
    This reverts commit 1b2c7fdb1fe5d34896f8226aa163630e13e3b912.

--- www/authoring/routing.php
+++ www/authoring/routing.php
@@ -11,7 +11,7 @@ include_once '../lrn_config.php';
 
 //alias(es) to eliminate the need for fully qualified classname(s) from sdk
 use LearnositySdk\Request\Init;
-
+use LearnositySdk\Utils\Uuid;
 
 //security object. timestamp added by SDK
 $security = [
@@ -19,23 +19,9 @@ $security = [
     'domain' => $domain
 ];
 
-
 //simple api request object for item list view
 $request = [
     'mode' => 'item_list',
-    'config' => [
-        'item_edit' => [
-            'item' => [
-                'reference' => [
-                    'show' => true,
-                    'edit' => true
-                ],
-                'dynamic_content' => true,
-                'shared_passage' => true,
-                'enable_audio_recording' => true
-            ]
-        ]
-    ],
     'user' => [
         'id' => 'demos-site',
         'firstname' => 'Demos',
@@ -52,16 +38,107 @@ $signedRequest = $Init->generate();
     <div class="jumbotron section">
         <div class="toolbar">
             <ul class="list-inline">
-                <li data-toggle="tooltip" data-original-title="Preview API Initialisation Object"><a href="#"  data-toggle="modal" data-target="#initialisation-preview" aria-label="Preview API Initialisation Object"><span class="glyphicon glyphicon-search"></span></a></li>
                 <li data-toggle="tooltip" data-original-title="Visit the documentation"><a href="https://support.learnosity.com/hc/en-us/categories/360000105358-Learnosity-Author" title="Documentation"><span class="glyphicon glyphicon-book"></span></a></li>
             </ul>
         </div>
         <div class="overview">
-        <h2>Maintenance Mode</h2>
-            <p>The Authoring Demos are currently undergoing maintenance and will return soon.</p>
+            <h2>Navigate Your Item Bank</h2>
+            <p>Programmatically search and navigate to existing content, as well as create new content, with linkable, bookmarkable, shareable URLs.</p>
+            <p>Our <a href="https://help.learnosity.com/hc/en-us/articles/360000755198-Building-Your-Own-Author-API-User-Journey-Using-navigate-">knowledge base article</a> demonstrates how easy it is to set up routing with the Author API by using the <a href="https://reference.learnosity.com/author-api/events/supported/navigate">'navigate' event</a> and the <a href="http://reference.learnosity.com/author-api/methods/authorApp/navigate">'navigate()' public method</a>.</p>
         </div>
     </div>
 
+
+    <!-- Container for the author api to load into -->
+    <div class="section pad-sml">
+        <div class="text-center">
+            <button class="btn btn-primary btn-navigate-item-new">New Item</button>
+            <button class="btn btn-primary btn-navigate-activity-new">New Activity</button>
+            <button class="btn btn-primary btn-navigate-widgets-new">New Question</button>
+            <button class="btn btn-primary btn-navigate-feature-new">New Feature</button>
+            <button class="btn btn-primary btn-navigate-items">List Items</button>
+            <button class="btn btn-primary btn-navigate-activities">List Activities</button>
+            <button class="btn btn-primary btn-navigate-widget-edit">Edit MCQ Question</button>
+        </div><br>
+        <!--    HTML placeholder that is replaced by API-->
+        <div id="learnosity-author"></div>
+    </div>
+
+
+    <!-- version of api maintained in lrn_config.php file -->
+    <script src="<?php echo $url_authorapi; ?>"></script>
+    <script>
+        var initializationObject = <?php echo $signedRequest; ?>;
+
+        //optional callbacks for ready
+        var callbacks = {
+            readyListener: function () {
+               console.log("Author API has successfully initialized.");
+
+                // Use navigate event to Log navigate routing
+                authorApp.on('navigate', function (e) {
+                    console.log('Navigate to: ' + e.data.location);
+                });
+
+               implementNavigate();
+            },
+            errorListener: function (err) {
+                console.log(err);
+            }
+        };
+
+        var authorApp = LearnosityAuthor.init(initializationObject, callbacks);
+
+        function implementNavigate () {
+            // Navigate to new item
+            $('.btn-navigate-item-new').click(function () {
+                authorApp.navigate(
+                    'items/new'
+                );
+            });
+            // Navigate to new activity
+            $('.btn-navigate-activity-new').click(function () {
+                authorApp.navigate(
+                    'activities/new'
+                );
+            });
+            // Navigate to new question
+            $('.btn-navigate-widgets-new').click(function () {
+                authorApp.navigate(
+                    'items/new/widgets/new'
+                );
+            });
+            // Navigate to new feature
+            $('.btn-navigate-feature-new').click(function () {
+                authorApp.navigate(
+                    'items/new/widgets/new/' + encodeURIComponent('{"widgetType":"features"}')
+                );
+            });
+            // Navigate to items list
+            $('.btn-navigate-items').click(function () {
+                authorApp.navigate(
+                    'items'
+                );
+            });
+            // Navigate to activities list
+            $('.btn-navigate-activities').click(function () {
+                authorApp.navigate(
+                    'activities'
+                );
+            });
+            // Navigate to edit MCQ question
+            $('.btn-navigate-widget-edit').click(function () {
+                authorApp.navigate(
+                    'items/new/widgets/new/' + encodeURIComponent(JSON.stringify({
+                        widgetTemplate: {
+                            template_reference: '9e8149bd-e4d8-4dd6-a751-1a113a4b9163'
+                        }
+                    }))
+                );
+            });
+        }
+    </script>
+
+
 <?php
-include_once 'views/modals/initialisation-preview.php';
 include_once 'includes/footer.php';
